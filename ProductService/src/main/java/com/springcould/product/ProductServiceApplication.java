53806c26fc546ba91620f8fb4e39792a6b8a7b3a
Sadanala <ssadanala@deloitte.com>
2020-10-07 21:01:33 +0530

    hystrix fault tolerance changes

--- ProductService/src/main/java/com/springcould/product/ProductServiceApplication.java
+++ ProductService/src/main/java/com/springcould/product/ProductServiceApplication.java
@@ -3,6 +3,8 @@ package com.springcould.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- ProductService/src/main/java/com/springcould/product/controller/ProductController.java
+++ ProductService/src/main/java/com/springcould/product/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springcould.product.entity.Coupon;
 import com.springcould.product.entity.Product;
 import com.springcould.product.repos.ProductRepo;
@@ -32,21 +33,27 @@ public class ProductController {
 
 	@Value(value = "${coupon.service.url}")
 	private String couponServiceUrl;
-	
+
 	@Autowired
 	private CouponClient couponClient;
 
+	@HystrixCommand(fallbackMethod = "sendErrorResponse")
 	@PostMapping(value = "/products")
 	public Product createProduct(@RequestBody Product product) {
 		/**
-		 *Using RestTemplate  
+		 * Using RestTemplate
 		 */
-		//Coupon coupon = restTemplate.getForObject(couponServiceUrl + product.getCouponCode(), Coupon.class);
+		// Coupon coupon = restTemplate.getForObject(couponServiceUrl +
+		// product.getCouponCode(), Coupon.class);
 		/**
 		 * Using FeignClient
 		 */
 		com.springcould.product.dto.Coupon coupon = couponClient.getCoupon(product.getCouponCode());
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return productRepo.save(product);
+	}
+
+	public Product sendErrorResponse(Product product) {
+		return product;
 	}
 }
